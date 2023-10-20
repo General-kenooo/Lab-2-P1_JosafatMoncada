86486f357ad08c17cbcc40cda0b9969b04e73613
General-kenooo <141855984+General-kenooo@users.noreply.github.com>
2023-10-20 15:43:15 -0600

    Add files via upload

---
+++ Lab2P1_JosafatMoncada_/src/lab2p1_josafatmoncada_/Lab2P1_JosafatMoncada_.java
@@ -0,0 +1,102 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab2p1_josafatmoncada_;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author monca
+ */
+public class Lab2P1_JosafatMoncada_ {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+         int numero =3;
+        double doble = 1.25;
+        boolean VoF = true;
+        char character = '&';
+        System.out.println("Elija el ejercicio: ");
+        numero = scanner.nextInt();
+          if (numero ==1){
+            Scanner leer = new Scanner (System.in);
+            System.out.println("ingrese primera longitud:");
+            int largo1 = leer.nextInt();
+            System.out.println("ingrese segunda longitud:");
+            int largo2 = leer.nextInt();
+            System.out.println("ingrese tercera longitud:");
+            int largo3 = leer.nextInt();
+            char reaps= 's';
+           if (largo1+largo2>largo3);
+            System.out.println("las longitudes forman un triangulo");
+           if (largo1+largo2<largo3);
+              System.out.println("las longitudes no forman un triangulo");
+         
+            
+              
+        }else if (numero ==2);
+          
+        
+    }        
+                 
+           
+       
+        
+}        
+        
+           
+           
+            
+            
+        
+                
+            
+         
+        
+        
+          
+        
+      
+            
+      
+        
+            
+         
+            
+    
+        
+        
+        
+        
+         
+
+       
+               
+      
+        
+                
+                
+           
+         
+        
+                
+                
+        
+           
+            
+              
+        
+               
+        
+        
+       
+        
+        
+   
+
+
